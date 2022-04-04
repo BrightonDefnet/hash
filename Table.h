2152ad595b1ec0a0e918dbce87d611398bc1f3bf
BrightonDefnet <475373@bsd48.org>
2022-04-03 17:05:33 -0700

    created random student generator

--- Table.h
+++ Table.h
@@ -9,16 +9,11 @@ class Table {
     public:
         Table(); //default constructor
         bool ADD(); //add a student to the table
-        bool PRINT(int); //print a student's info
-        bool DELETE(int); //delete a student from the list
+        bool PRINT(); //print a student's info
+        bool DELETE(); //delete a student from the list
 
     private:
-        Node* sizeOne[100] = {nullptr};
-        Node* sizeTwo[200] = {nullptr};
-        Node* sizeThree[400] = {nullptr};
-        Node* sizeFour[800] = {nullptr};
-        Node* sizeFive[1600] = {nullptr};
-        int current = 1;
+        Node* table[100] = {nullptr};
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,23 +5,65 @@
  */
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 #include "Node.h"
 #include "Table.h"
 #include "Student.h"
 using namespace std;
 
-int main() {
-    Table* t = new Table();
-    t->ADD();
-    t->ADD();
+int id = 0;
+void genStudent();
 
-    int id;
-    cout << "\nenter id to search for"  << endl;
-    cin >> id;
-    bool b = t->PRINT(id);
-    if(b) {
-        cout << "\nstudent found!" << endl;
-    } else {
-        cout << "\nstudent not found" << endl;
-    }
+int main() {
 }
+
+///randomly generate a new student
+void genStudent() {
+    string line;
+    vector<string> first;
+    vector<string> last;
+    //get first name
+    ifstream firstfile("first.txt");
+    if(firstfile.is_open()) {
+        while(getline(firstfile, line)) {
+            first.push_back(line);
+        }
+        firstfile.close();
+    }
+    //get last name
+    ifstream lastfile("last.txt");
+    if(lastfile.is_open()) {
+        while(getline(lastfile, line)) {
+            last.push_back(line);
+        }
+        lastfile.close();
+    }
+    //create student
+    string firstname = first.at(rand() % 20);
+    string lastname = last.at(rand() % 20);
+    int sid = id;
+    id += 1;
+    float gpa = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/4));
+    cout << firstname << endl;
+    cout << lastname << endl;
+    cout << sid << endl;
+    cout << gpa << "\n" << endl;
+}
+
+/*
+//get student data
+string first;
+string last;
+int id;
+float gpa;
+cout << "\nenter first name" << endl;
+cin >> first;
+cout << "\nenter last name" << endl;
+cin >> last;
+cout << "\nenter student id" << endl;
+cin >> id;
+cout << "\nenter student gpa" << endl;
+cin >> gpa;
+*/
